23d3797492308b27604fde91ac9dbfb9294f2da9
Võ Tấn Đạt <91039754+VNKaiSer@users.noreply.github.com>
2023-09-09 16:52:10 +0700

    add : connect db, acount respository

--- src/main/java/vn/edu/iuh/fit/lab_week01/respositories/AccountRespository.java
+++ src/main/java/vn/edu/iuh/fit/lab_week01/respositories/AccountRespository.java
@@ -1,5 +1,15 @@
 package vn.edu.iuh.fit.lab_week01.respositories;
 
+import java.sql.Connection;
+
 public class AccountRespository {
+    private Connection connection;
+
+    public AccountRespository() throws Exception {
+        connection = DBConnect.getInsance().getConn();
+    }
+
+
+
 
 }
--- src/main/java/vn/edu/iuh/fit/lab_week01/respositories/DBConnect.java
+++ src/main/java/vn/edu/iuh/fit/lab_week01/respositories/DBConnect.java
@@ -4,26 +4,31 @@ import vn.edu.iuh.fit.lab_week01.config.DBConfig;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnect implements DBConfig{
     private static DBConnect insance;
-    private Connection conn = null;
+    private Connection conn;
 //    private
-    public DBConnect() {
-
-    }
-
-    public void connection() throws Exception {
-        if(insance == null){
-            insance = new DBConnect();
-        }
+    public DBConnect() throws ClassNotFoundException, SQLException {
         Class.forName("org.mariadb.jdbc.Driver");
         conn = DriverManager.getConnection(
                 DB_URL, USER, PASS);
         System.out.println("Connected database successfully...");
     }
 
-    public static DBConnect getInsance() {
+    public static DBConnect getInsance() throws Exception {
+        if(insance == null){
+            insance = new DBConnect();
+        }
         return insance;
     }
+
+    public Connection getConn() {
+        return conn;
+    }
+
+//    public static void main(String[] args) throws Exception {
+//        DBConnect.getInsance().getConn();
+//    }
 }
